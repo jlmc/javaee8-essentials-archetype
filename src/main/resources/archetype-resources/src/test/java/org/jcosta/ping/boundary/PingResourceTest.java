3b1306b80601810cecbecb7dce68c739cc0d18c2
Joao Costa <costajlmpp@gmail.com>
2017-10-22 00:28:53 +0100

    update artifactId

--- src/main/resources/archetype-resources/src/test/java/org/jcosta/ping/boundary/PingResourceTest.java
+++
@@ -1,3 +0,0 @@
-package org.jcosta.ping.boundary;
-
-public class PingResourceTest {}
--- target/classes/archetype-resources/src/main/java/org/costajlmpp/ping/boundary/PingResource.java
+++ target/classes/archetype-resources/src/main/java/org/costajlmpp/ping/boundary/PingResource.java
@@ -1,4 +1,4 @@
-package org.jcosta.ping.boundary;
+package org.costajlmpp.ping.boundary;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
